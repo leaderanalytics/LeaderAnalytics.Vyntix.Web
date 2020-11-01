f69a4e211719d5a01ec443bfcc5e57cf8890746d
Sam <sam.wheat@outlook.com>
2020-11-01 08:21:54 -0800

    Fix AppConfig host URL

--- LeaderAnalytics.Vyntix.Web/ClientApp/src/Config/AppConfig.prod.tsx
+++ LeaderAnalytics.Vyntix.Web/ClientApp/src/Config/AppConfig.prod.tsx
@@ -2,7 +2,7 @@
     //----------------------------------------------------------------------
     // This file is copied from src/Config at build time.   See .csproj file
     //----------------------------------------------------------------------
-    public static host: string = "https://vyntix.com";
+    public static host: string = "https://vyntix.com/";
     public static loginScopes: any = { scopes: ['openid', 'profile'] };
     public static StripeApiKey : string = "pk_live_51H4XDXLYasEZvdvrGjxforOOCxAWCrOxLPoJOjeFvFI0DBxCjEEWVrpecKtDtEbxkFTf3ZQSK2AcuGAxP5xPER3c00VFCBO4Sw";
     public static StripeClientId: string = "";
