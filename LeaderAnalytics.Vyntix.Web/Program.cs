86ffa0bdaa25727208bf8d57bb50704971ff742d
Sam <sam.wheat@outlook.com>
2021-01-07 17:54:51 -0800

    Add Serilog.AspNetCore

--- LeaderAnalytics.Vyntix.Web/Program.cs
+++ LeaderAnalytics.Vyntix.Web/Program.cs
@@ -45,9 +45,10 @@ namespace LeaderAnalytics.Vyntix.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            .UseSerilog()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
